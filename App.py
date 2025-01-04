6fcea24d32c6fd4ff46a0b03f312207cbe1bd9a0
sabrin235 <pa235amin@gmail.com>
2025-01-04 12:45:14 -0500

    Update App.py

--- App.py
+++ App.py
@@ -19,8 +19,7 @@ def caesar(message, offset):
 
 # Streamlit app layout
 st.title("Sabrin's Cipher App")
-st.write("The Ceaser Cipher can encrypt your text and shift it to 1-25 values, although the default shift value is 5. When you're done, click "Encrypt Now!" to get your encryption."
-)
+st.write("The Ceaser Cipher App allows you to encrypt normal text by shifting the values. You can shift values from 1-25, but 5 is the default shift value. When you're done, press the Encrypt Now! button to get your encryption.")
 # Input for plain text
 text = st.text_input("Enter the text to be encrypted:")
 
@@ -28,7 +27,7 @@ text = st.text_input("Enter the text to be encrypted:")
 shift = st.number_input("Enter the shift value (1-25):", min_value=1, max_value=25, value=5)
 
 # Button to encrypt
-if st.button("Encrypt"):
+if st.button("Encrypt Now!"):
     if text:
         encrypted_text = caesar(text, shift)
         st.write('**Plain text:**', text)
